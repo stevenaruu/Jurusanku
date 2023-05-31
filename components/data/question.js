48ce5a7f97929d8829c06bda43b9f09c6922ec99
Steven Forsythia <stvnhd164@gmail.com>
2023-05-31 22:03:37 +0700

    feat: add 10 new questions while taking test

--- components/data/question.js
+++ components/data/question.js
@@ -1,15 +1,27 @@
 export default data = [
     {
-        question: "Apakah kamu mengikuti berita tentang teknologi terkini?",
+        question: "Apakah anda mengikuti berita terkini terkait teknologi coding?",
     },
     {
         question: "Apakah anda mengerti dan menyukai coding/programming?",
     },
     {
+        question: "Apa pendapat anda tentang pekerjaan berbau coding?",
+    },
+    {
+        question: "Apakah anda pernah mencoba coding sebelum mengikuti tes ini?",
+    },
+    {
         question: "Apakah anda suka konsep pembuatan game (design stage, coding)?",
     },
     {
-        question: "Seberapa tertarik anda dengan industri yang bergerak dibidang game?",
+        question: "Seberapa tertarik anda dengan industri yang bergerak di bidang game?",
+    },
+    {
+        question: "Apakah anda seseorang yang sering bermain game?",
+    },
+    {
+        question: "Apakah anda tertarik untuk bermain game di level kompetitif?",
     },
     {
         question: "Apakah anda tertarik melakukan proses jual-beli?",
@@ -18,15 +30,33 @@ export default data = [
         question: "Apakah anda suka menghitung ekonomi (laba,netto,brutto)?",
     },
     {
+        question: "Apakah anda suka dengan proses mengembangkan bisnis?",
+    },
+    {
+        question: "Apakah anda merasa anda punya leadership yg kuat?",
+    },
+    {
         question: "Apakah anda tertarik dalam pembelajaran bahasa?",
     },
     {
-        question: "Bagaiamanakah seni bahasa menurut opini anda?",
+        question: "Bagaimanakah seni bahasa menurut opini anda?",
     },
     {
-        question: "Apakah anda tipe orang yang peduli terhadap hewan?",
+        question: "Berapa jumlah bahasa yang dapat anda kuasai?",
     },
     {
-        question: "Bagaimana ketertarikan anda dengan pelajaran biologi?",
-    }
+        question: "Apakah anda tertarik untuk mempelajari bahasa negara lain",
+    },
+    {
+        question: "Apakah anda tipe orang yang menyayangi hewan?",
+    },
+    {
+        question: "Bagaimana ketertarikan anda dengan biologi?",
+    },
+    {
+        question: "Apakah anda merasa lebih bahagia bila di sekitar hewan?",
+    },
+    {
+        question: "Apakah anda suka mempelajari hewan pada pelajaran biologi?",
+    },
 ]
